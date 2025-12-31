e9978e8355250cc92ceab619ba8342311837e05c
Doomzy <mohamed_abdulhamid5@outlook.com>
2025-12-31 23:33:55 +0200

    style: update layout and global styles
    - Update root layout with sidebar provider and Sonner toaster
    - Modify global CSS styles
    - Configure layout for dashboard application

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app_sidebar";
+import { Toaster } from "@/components/ui/sonner";
+import { BreadcrumbsBar } from "@/components/breadcrumbs-bar";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,10 +17,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body>
+        <SidebarProvider>
+          <AppSidebar />
+          <div className="w-full bg-background text-accent-foreground">
+            <div className="flex gap-4 items-center">
+              <SidebarTrigger />
+              <BreadcrumbsBar />
+            </div>
+            <main className="h-full px-4 py-4">{children}</main>
+          </div>
+        </SidebarProvider>
+        <Toaster />
       </body>
     </html>
   );
